b931423f9f151c0d57c57a17a3f306566cb63062
martin <martin@Deep-Blue>
2014-01-22 19:44:15 -0300

    AnuncioDAO Cambios en método find.
    Decorador agregado

---
+++ DisenoDeSistemas/src/servicios/Decorador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servicios;
+
+import entidades.Anuncio;
+
+/**
+ *
+ * @author martin
+ */
+public abstract class Decorador extends Anuncio{
+    
+    public void agregar (Object elemento){
+        
+    }
+    
+    public void comentar (String texto){
+        
+    }
+    
+}
